d27ed63dea961da8c1528ec59b0d4e58e9b15cff
maulidaprina <mldaprna@gmail.com>
2024-10-28 06:16:33 +0700

    commit

---
+++ Star12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Star12 {
+    public static void main(String[] args) {
+       
+        Scanner Input12 = new Scanner(System.in);
+
+int N;
+
+System.out.print("Masukkan nilai N = ");
+N = Input12.nextInt();
+
+for(int i=1; i<=N; i++) {
+    System.out.print("*");
+}
+
+
+    }
+}
